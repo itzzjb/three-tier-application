66e52b36aac3292efd95c63fecdbc3274731c2af
Januda Bethmin <desilvabethmin@gmail.com>
2024-10-21 16:21:09 +0530

    added ec2 instances and security groups

---
+++ infrastructure/datasources.tf
@@ -0,0 +1,9 @@
+# getting the latest version of ubuntu ami via datasource
+data "aws_ami" "ami" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+}
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,15 +8,15 @@ resource "aws_vpc" "vpc" {
 
 # creating the internet gateway for the vpc
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "aws_vpc.vpc.id"
+  vpc_id = aws_vpc.vpc.id
   tags = {
     Name = "app-internet-gateway"
   }
 }
 
 # creating the public subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = "aws_vpc.vpc.id"
+resource "aws_subnet" "public-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-2a" # availabilty zone is required for subnets
   map_public_ip_on_launch = true         # this will make this a public subnet
@@ -26,8 +26,8 @@ resource "aws_subnet" "public_subnet" {
 }
 
 # creating the private subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id            = "aws_vpc.vpc.id"
+resource "aws_subnet" "private-subnet" {
+  vpc_id            = aws_vpc.vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-2b"
   tags = {
@@ -36,15 +36,85 @@ resource "aws_subnet" "private_subnet" {
 }
 
 # creating the route table
-resource "aws_route_table" "route_table" {
-    vpc_id = aws_vpc.vpc.id
-    tags = {
-        Name = "app-route-table"
-    }
+resource "aws_route_table" "route-table" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "app-route-table"
+  }
 }
 
-# creating a route (entry in the route table)
+# creating security group for the public instance
+resource "aws_security_group" "public-security-group" {
+  name        = "app-public-security-group"
+  description = "this security group allows ssh, http and https"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "app-public-security-group"
+  }
+}
 
+# creating security group for the private instance
+resource "aws_security_group" "private-security-group" {
+  name        = "private-security-group"
+  description = "this security group alloes only ssh"
+  vpc_id      = aws_vpc.vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "app-private-security-group"
+  }
+}
 
+# creating the ec2 instance in the public subnet
+resource "aws_instance" "public-instance" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.ami.id
+  subnet_id     = aws_subnet.public-subnet.id
+  tags = {
+    Name = "app-public-instance"
+  }
+}
 
-
+# creating the ec2 instance in the private subnet
+resource "aws_instance" "private-instance" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.ami.id
+  subnet_id     = aws_subnet.private-subnet.id
+  tags = {
+    Name = "app-private-instance"
+  }
+}
