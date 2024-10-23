4503be83387c1526df45ea743250bf56e06cb069
Januda Bethmin <desilvabethmin@gmail.com>
2024-10-23 13:51:35 +0530

    output format changes

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -1,23 +1,23 @@
-output "vpc_id" {
+output "vpc-id" {
   value = aws_vpc.vpc.id
 }
 
-output "public_subnet_id" {
+output "public-subnet-id" {
   value = aws_subnet.public-subnet.id
 }
 
-output "private_subnet_id" {
+output "private-subnet-id" {
   value = aws_subnet.private-subnet.id
 }
 
-output "private_subnet_2_id" {
+output "private-subnet-2-id" {
   value = aws_subnet.private-subnet-2.id
 }
 
-output "public_security_group_id" {
+output "public-security-group-id" {
   value = aws_security_group.public-security-group.id
 }
 
-output "private_security_group_id" {
+output "private-security-group-id" {
   value = aws_security_group.private-security-group.id
 }
