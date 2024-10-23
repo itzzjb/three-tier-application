27c532a745dc9de80b861aaa79915e8faabcec21
Januda Bethmin <desilvabethmin@gmail.com>
2024-10-23 13:53:12 +0530

    structured the code done

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,13 +1,9 @@
 module "network" {
   source = "../../modules/network"
-  # No variables needed for now
 }
 
 module "compute" {
   source              = "../../modules/compute"
-  vpc_id              = module.network.vpc_id
-  public_subnet_ids   = module.network.public_subnet_ids
-  private_subnet_ids  = module.network.private_subnet_ids
 }
 
 module "database" {
@@ -15,6 +11,4 @@ module "database" {
   database_name       = var.database_name
   database_username   = var.database_username
   database_password   = var.database_password
-  vpc_id              = module.network.vpc_id
-  private_subnet_ids  = module.network.private_subnet_ids
 }
