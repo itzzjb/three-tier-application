281c6aba7e8b67d2baf7791ff24ff5d67c1da395
Januda Bethmin <desilvabethmin@gmail.com>
2024-10-24 16:24:24 +0530

    used s3 bucket backend

--- infrastructure (not-moduled)/providers.tf
+++ infrastructure (not-moduled)/providers.tf
@@ -1,4 +1,13 @@
 terraform {
+  # we are going to remote state file stored in amazon s3 instead of local state file
+  # we need to use the backend block for that
+  backend "s3" {
+    bucket = "itzzjb-terraform-bucket"
+    region = "us-east-2"
+    key = "key/terraform.tfstate" # name of the file that need to be created 
+    # key/ is just a directory inside the s3 bucket
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -1,4 +1,12 @@
 terraform {
+  # we are going to remote state file stored in amazon s3 instead of local state file
+  # we need to use the backend block for that
+  backend "s3" {
+    bucket = "itzzjb-terraform-bucket"
+    region = "us-east-2"
+    key = "dev/key/terraform.tfstate" # name of the file that need to be created 
+    # dev/key/ are just a directories inside the s3 bucket
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
